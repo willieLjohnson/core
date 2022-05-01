7437e57f2d3dec091f20dfa063f653cb12c0a986
Willie Johnson <liwa.johnson@gmail.com>
2022-05-01 06:43:25 -0400

    update core.py: add script that converts uploaded videos into markdown files

--- core.py
+++ core.py
@@ -13,9 +13,24 @@ class Video:
     thumbnail: string
     title: string
     description: string
+    date: string
     
 
 for video in videos:
+    print(video)
     if "video" in video["id"]["kind"]:
-        vid = Video(video["id"]["videoId"], video["snippet"]["thumbnails"]["high"]["url"], video["snippet"]["title"], video["snippet"]["description"])
+        vid = Video(video["id"]["videoId"], video["snippet"]["thumbnails"]["high"]["url"], video["snippet"]["title"], video["snippet"]["description"], video["snippet"]["publishedAt"])
         print(vid)
+        with open(f"out/{vid.date}_{vid.videoId}.md", 'a') as f:
+            f.writelines(
+                line + "\n" for line in [
+                    "---",
+                    "layout: video", 
+                    f"title: {vid.title}",
+                    f"imageurl: {vid.thumbnail}",
+                    f"videourl: https://www.youtube.com/embed/{vid.videoId}",
+                    f"published-on: https://www.youtube.com/embed/{vid.videoId}",
+                    "---\n",
+                    f"{vid.description}",
+                ]
+            )
