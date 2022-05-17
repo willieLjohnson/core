c89b628f96b8ec0690a71c92b5e3e103e2685231
Willie Johnson <liwa.johnson@gmail.com>
2022-05-16 22:29:45 -0400

    update formatting: format now matches jekyll post date format

--- core.py
+++ core.py
@@ -1,6 +1,10 @@
 import string
 import requests
 from dataclasses import dataclass
+from datetime import datetime
+from dateutil import parser
+
+
 
 response = requests.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDRiD0RkKyffuk6uptt_C5FCmYY-mxsKDM&channelId=UCiXhjiUOoJzPg3GVAc54YWg&part=snippet,id&order=date&maxResults=20')
 
@@ -20,16 +24,19 @@ for video in videos:
     print(video)
     if "video" in video["id"]["kind"]:
         vid = Video(video["id"]["videoId"], video["snippet"]["thumbnails"]["high"]["url"], video["snippet"]["title"], video["snippet"]["description"], video["snippet"]["publishedAt"])
+        date = parser.parse(vid.date)
         print(vid)
-        with open(f"out/{vid.date}_{vid.videoId}.md", 'a') as f:
+        with open(f"out/{date.strftime('%Y-%m-%d')}-{vid.videoId}.md", 'a') as f:
             f.writelines(
                 line + "\n" for line in [
                     "---",
                     "layout: video", 
-                    f"title: {vid.title}",
+                    f"title: '{vid.title}'",
                     f"imageurl: {vid.thumbnail}",
                     f"videourl: https://www.youtube.com/embed/{vid.videoId}",
-                    f"published-on: https://www.youtube.com/embed/{vid.videoId}",
+                    "published: true",
+                    f"updated-on: {vid.date}",
+                    "type: video",
                     "---\n",
                     f"{vid.description}",
                 ]
